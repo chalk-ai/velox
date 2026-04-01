bdbb726bfab29503b7a6cffbc48a34013af19560
Colin Schultz <colin.r.schultz@gmail.com>
2026-03-31 19:10:59 -0700

    fix

--- velox/expression/Expr.cpp
+++ velox/expression/Expr.cpp
@@ -1979,7 +1979,7 @@ ExprSet::ExprSet(
           execCtx->queryCtx()
               ->queryConfig()
               .exprAdaptiveCpuSamplingMaxOverheadPct()) {
-  exprs_ = compileExpressions(sources, execCtx, this, enableConstantFolding);
+  exprs_ = compileExpressions(sources, execCtx_, this, enableConstantFolding);
   if (lazyDereference_) {
     validateLazyDereference(exprs_);
   }
